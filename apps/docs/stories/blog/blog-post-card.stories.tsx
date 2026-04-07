087defc57a2f47b56a3770aad2a4d15169bc83d4
Raffortville <raffihaycan@gmail.com>
2026-04-07 11:47:33 +0400

    feat: update blog card feature

--- apps/docs/stories/blog/blog-post-card.stories.tsx
+++ apps/docs/stories/blog/blog-post-card.stories.tsx
@@ -4,24 +4,55 @@ import { BlogPostCard } from '@secrecy/ui/components/features/blog/card/blog-pos
 const meta: Meta<typeof BlogPostCard> = {
 	component: BlogPostCard,
 	title: 'Features/BlogPostCard',
+	argTypes: {
+		size: {
+			control: 'select',
+			options: ['sm', 'md', 'lg'],
+		},
+	},
 };
 
 export default meta;
 
 type Story = StoryObj<typeof BlogPostCard>;
 
+const baseArgs = {
+	tagLabel: 'Engineering',
+	img: {
+		src: '/image/video-poster.jpg',
+	},
+	title: 'Design System Update',
+	description: 'A short overview of recent component improvements.',
+	text: '5 min read',
+	link: {
+		href: '#read-more',
+		label: 'Read more',
+	},
+};
+
 export const Default: Story = {
 	args: {
-		tagLabel: 'Engineering',
-		img: {
-			src: '/image/video-poster.jpg',
-		},
-		title: 'Design System Update',
-		description: 'A short overview of recent component improvements.',
-		text: '5 min read',
-		link: {
-			href: '#read-more',
-			label: 'Read more',
-		},
+		...baseArgs,
+	},
+};
+
+export const Small: Story = {
+	args: {
+		...baseArgs,
+		size: 'sm',
+	},
+};
+
+export const Medium: Story = {
+	args: {
+		...baseArgs,
+		size: 'md',
+	},
+};
+
+export const Large: Story = {
+	args: {
+		...baseArgs,
+		size: 'lg',
 	},
 };
